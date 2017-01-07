17398ed19dee9760fb8e03bd73c493208bb3aa2f
Naoto Nakazato <nnakazat@yahoo-corp.jp>
2017-01-07 12:34:54 +0900

    Add ImageView into Activity

--- app/src/main/java/com/oxsoft/irasutoya/MainActivity.java
+++ app/src/main/java/com/oxsoft/irasutoya/MainActivity.java
@@ -2,6 +2,9 @@ package com.oxsoft.irasutoya;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageKeyboardSupportEditText imageKeyboardSupportEditText = (ImageKeyboardSupportEditText) findViewById(R.id.image_keyboard_support_edit_text);
+        final ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        imageKeyboardSupportEditText.getUri().subscribe(uri -> Glide.with(this).load(uri).into(imageView));
     }
 }
